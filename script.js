8dbe2f86e9029b06cc05fa519496be1f8a072953
yanakin394 <yana.germatskaya@gmail.com>
2023-06-03 13:44:38 +0300

    fetch

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ const container = document.querySelector('.container');
 
 function getPost () { 
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(json => {
-        json.forEach((elem) => {
-            console.log(elem.title);
-            console.log(elem.body);
-            const wrapper = document.createElement('div');
-            wrapper.classList.add('post_wrapper');
+        .then(response => response.json())
+        .then(json => {
+            json.forEach((elem) => {
+                console.log(elem.title);
+                console.log(elem.body);
+                const wrapper = document.createElement('div');
+                wrapper.classList.add('post_wrapper');
 
-            wrapper.innerHTML = `
-            <div class="title">Заголовок: ${elem.title}</div>
-            <div class="post">Статья: ${elem.body}</div>
-            `; 
-            container.appendChild(wrapper);
-
+                wrapper.innerHTML = `
+                <div class="title">Заголовок: ${elem.title}</div>
+                <div class="post">Статья: ${elem.body}</div>
+                `; 
+                container.appendChild(wrapper);
+            })
         })
-    });
+        .catch((err) => {
+            console.log('Error!' + err);
+        })
 }
 
 getPost();
